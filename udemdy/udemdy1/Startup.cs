fafb2142da0e6fd78ce5f5dd4e5706b6757a243a
WolvesComeIn <wolvescomein@gmail.com>
2017-05-30 07:03:51 -0400

    Initial Commit

---
+++ udemdy/udemdy1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(udemdy1.Startup))]
+namespace udemdy1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
